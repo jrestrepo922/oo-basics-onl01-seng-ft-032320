154040b3d4b95f3a9b6ce0df6bb40a409b7b135c
JUAN M RESTREPO <tinto922@gmail.com>
2020-03-31 11:30:08 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,50 @@
+class Book 
+  def initialize(tittle)
+    @tittle = tittle
+  end 
+  
+  def tittle
+    @tittle
+  end 
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#learn spec/01_book_spec.rb
 
 
